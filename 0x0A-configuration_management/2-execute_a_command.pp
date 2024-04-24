1256349cb8c5a61aff61e95690ca9aa92620b363
Hajar-developer <hajarelfadil2001@gmail.com>
2024-04-24 11:26:11 -0700

    configuration management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,9 @@
-# killing a process named `killmenow`
-exec { 'kill_killmenow':
-  command => '/bin/pkill killmenow',
+#!/usr/bin/env puppet
+# Using Puppet to create a manifest that kills a process named killmenow.
+
+exec { 'kill':
+  command   => 'pkill -f killmenow',
+  path      => ['/usr/bin', '/usr/sbin'],
+  returns   => ['0', '1'],
+  logoutput => true,
 }
